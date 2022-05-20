a39c96e399c5841bde20cdc43d44c6878017be29
Laambert <98312235+Laambert@users.noreply.github.com>
2022-05-20 23:49:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ let galery = document.getElementById("galery");
 let galeryIndex = 0;
 
 let galeryImages = [
-  "using_images/australia.jpg", 
-  "using_images/coloseum.webp", 
-  "using_images/garden.jpg",
-  'using_images/default.webp'
+  "australia.jpg", 
+  "coloseum.webp", 
+  "garden.jpg",
+  'default.webp'
 ];
 
 buy.addEventListener('click', function onClick(event) {
@@ -19,7 +19,7 @@ buy.addEventListener('click', function onClick(event) {
 left.addEventListener('click', function onClick(event) {
   if(galeryIndex == 0){
     galeryIndex = galeryImages.length;
-    galery.style.backgroundImage='url("using_images/default.webp")';
+    galery.style.backgroundImage='url("default.webp")';
   }
     else{
       galeryIndex -= 1;
@@ -32,7 +32,7 @@ left.addEventListener('click', function onClick(event) {
 
     if(galeryIndex == galeryImages.length){
       galeryIndex = 0;
-      galery.style.backgroundImage='url("using_images/default.webp")';
+      galery.style.backgroundImage='url("default.webp")';
     }
       else{
         galery.style.backgroundImage="url("+ galeryImages[galeryIndex]+")";
