92da46e21bcb482066396cfcf0228d0a8daff64d
Laambert <98312235+Laambert@users.noreply.github.com>
2022-05-21 15:06:46 +0200

    Add files via upload

--- zombie.js
+++ zombie.js
@@ -1,4 +1,5 @@
-let image = document.getElementsByClassName("image")[0];
+const image = document.getElementsByClassName("image")[0];
+const text = document.getElementsByClassName("caption")[0];
 
 const BackgroundAnimation = image.animate([
     {
@@ -29,7 +30,36 @@ const BackgroundAnimation = image.animate([
         transform: "scale(2.0)",
     }
 ], {
-    duration: 60000, 
+    duration: 50000, 
     easing: "ease",
     iterations: Infinity,
 });
+
+const TextAnimation = text.animate([
+    {
+        transform: "scale(1.4)",
+        transform: "rotate(0deg)"
+    },
+    {
+        transform: "scale(1.1)",
+        transform: "rotate(2deg)"
+    },
+    {
+        transform: "scale(1.1)",
+        transform: "rotate(0deg)"
+    },
+    {
+        transform: "scale(1.1)",
+        transform: "rotate(-2deg)"
+    },
+    {
+        transform: "scale(1.4)",
+        transform: "rotate(0deg)"
+    }
+], {
+    duration: 8000, 
+    easing: "linear",
+    iterations: Infinity,
+});
+
+
