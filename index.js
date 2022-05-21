24c17830af79e6be0247bd0f7d67e78bd2c24496
Laambert <98312235+Laambert@users.noreply.github.com>
2022-05-21 16:01:03 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,38 +5,57 @@ let galery = document.getElementById("galery");
 let galeryIndex = 0;
 
 let galeryImages = [
-  "australia.jpg", 
-  "coloseum.webp", 
-  "garden.jpg",
-  'default.webp'
+  "australia.jpg",
+  "coloseum.webp",
+  "garden.jpg"
 ];
+
+function anim(){
+  const Animation = galery.animate([
+    {
+      opacity: "0.5"
+    },
+    {
+      opacity: "1",
+    }
+    
+  ], {
+    duration: 800,
+    easing: "linear",
+    iterations: 1,
+  });
+}
 
 buy.addEventListener('click', function onClick(event) {
 
-    event.target.style.backgroundColor = 'background: rgba(184, 17, 46, 1);';
-  });
+  event.target.style.backgroundColor = 'background: rgba(184, 17, 46, 1);';
+});
 
 left.addEventListener('click', function onClick(event) {
-  if(galeryIndex == 0){
+  if (galeryIndex == 0) {
     galeryIndex = galeryImages.length;
-    galery.style.backgroundImage='url("default.webp")';
+    anim();
+    galery.style.backgroundImage = 'url("default.webp")';
   }
-    else{
-      galeryIndex -= 1;
-      galery.style.backgroundImage="url("+ galeryImages[galeryIndex]+")";
+  else {
+    galeryIndex -= 1;
+    anim();
+    galery.style.backgroundImage = "url(" + galeryImages[galeryIndex] + ")";
 
-    }
-  });
+  }
+});
 
-  right.addEventListener('click', function onClick(event) {
+right.addEventListener('click', function onClick(event) {
 
-    if(galeryIndex == galeryImages.length){
-      galeryIndex = 0;
-      galery.style.backgroundImage='url("default.webp")';
-    }
-      else{
-        galery.style.backgroundImage="url("+ galeryImages[galeryIndex]+")";
-        galeryIndex += 1;
-      }
-  });
+  if (galeryIndex == galeryImages.length) {
+    galeryIndex = 0;
+    anim();
+    galery.style.backgroundImage = 'url("default.webp")';
+  }
+  else {
+    anim();
+    galery.style.backgroundImage = "url(" + galeryImages[galeryIndex] + ")";
+    galeryIndex += 1;
+  }
+});
 
